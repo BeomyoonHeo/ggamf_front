44b13dc9b2e503972e2d632d895d979befffa57b
BeomyoonHeo <gqd0512@naver.com>
2022-11-26 21:23:21 +0900

    텍스트필드 생성 완료

---
+++ lib/views/pages/login_user/components/text_field_container.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:ggamf_front/core/color.dart';
+
+class TextFieldContainer extends StatelessWidget {
+  final Widget child;
+  const TextFieldContainer({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+      width: size.width * 0.8 - 20,
+      decoration: BoxDecoration(
+        color: kPrimaryColor,
+        borderRadius: BorderRadius.circular(30),
+      ),
+      child: child,
+    );
+  }
+}
