eb8449df2f84c37f153ccb9bc0a848f1c98a550c
BeomyoonHeo <gqd0512@naver.com>
2022-12-01 12:50:31 +0900

    탭바 커스터마이징 완료

--- lib/views/pages/recommend_ggamef/recommend_ggamf_list/recommend_ggamf_list.dart
+++ lib/views/pages/recommend_ggamef/recommend_ggamf_list/recommend_ggamf_list.dart
@@ -1,65 +1,100 @@
 import 'package:flutter/material.dart';
 
 class RecommendGgamfList extends StatefulWidget {
-  const RecommendGgamfList({Key? key}) : super(key: key);
-
+  const RecommendGgamfList({super.key});
   @override
-  State<RecommendGgamfList> createState() => _RecommendGgamfListState();
+  _RecommendGgamfListState createState() => _RecommendGgamfListState();
 }
 
 class _RecommendGgamfListState extends State<RecommendGgamfList>
-    with TickerProviderStateMixin {
+    with SingleTickerProviderStateMixin {
   late TabController _tabController;
 
   @override
   void initState() {
-    super.initState();
     _tabController = TabController(length: 2, vsync: this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _tabController.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [
-        Container(
-          child: TabBar(controller: _tabController, tabs: [
-            Container(
-              height: 80,
-              child: Text('추천 껨프'),
-            ),
-            Container(
-              height: 80,
-              child: Text('껨프 요청'),
-            ),
-          ]),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Text(
+          '추천 껨프',
         ),
-        Expanded(
-            child: TabBarView(
-          controller: _tabController,
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
           children: [
+            // give the tab bar a height [can change hheight to preferred height]
             Container(
-              color: Colors.yellow[200],
-              alignment: Alignment.center,
-              child: Text(
-                'Tab1 View',
-                style: TextStyle(
-                  fontSize: 30,
+              height: 45,
+              decoration: BoxDecoration(
+                color: Colors.grey[300],
+                borderRadius: BorderRadius.circular(
+                  25.0,
                 ),
               ),
-            ),
-            Container(
-              color: Colors.green[200],
-              alignment: Alignment.center,
-              child: Text(
-                'Tab2 View',
-                style: TextStyle(
-                  fontSize: 30,
+              child: TabBar(
+                controller: _tabController,
+                // give the indicator a decoration (color and border radius)
+                indicator: BoxDecoration(
+                  borderRadius: BorderRadius.circular(
+                    25.0,
+                  ),
+                  color: Colors.black,
                 ),
+                labelColor: Colors.white,
+                unselectedLabelColor: Colors.black,
+                tabs: [
+                  // first tab [you can add an icon using the icon property]
+                  Tab(
+                    text: '추천 껨프',
+                  ),
+
+                  // second tab [you can add an icon using the icon property]
+                  Tab(
+                    text: '껨프 요청',
+                  ),
+                ],
+              ),
+            ),
+            // tab bar view here
+            Expanded(
+              child: TabBarView(
+                controller: _tabController,
+                children: [
+                  // first tab bar view widget
+                  Center(
+                    child: Text(
+                      '추천 껨프',
+                      style: TextStyle(fontSize: 25, fontFamily: 'NanumSquare'),
+                    ),
+                  ),
+
+                  // second tab bar view widget
+                  Center(
+                    child: Text(
+                      '껨프 요청',
+                      style: TextStyle(fontSize: 25, fontFamily: 'NanumSquare'),
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
-        ))
-      ]),
+        ),
+      ),
     );
   }
 }
