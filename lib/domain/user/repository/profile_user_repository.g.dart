321e638bddb99f49d81fe300849302d0575d0ec5
bigjang77 <112357301+bigjang77@users.noreply.github.com>
2022-12-08 16:23:11 +0900

    [최영훈]내 프로필 mvvm패턴 구현
    * 배경 그라데이션 완료
    
    * 텍스트필드 생성 완료
    
    * 사용자 입력 폼 완료
    
    * 로그인 화면 구현 완료
    
    * 인증번호받기 버튼 완료
    
    * 임시저장
    
    * 드롭다운 버튼 생성 완료
    
    * 드롭다운 버튼 수정
    
    * 전화번호 박스 max length 수정
    
    * 임시저장
    
    * 모달창 stateful 적용 완료
    
    * 회원가입 화면 구성 완료
    
    * 바텀 네비게이션 바 common으로 처리
    
    * 네비게이션바 수정 및 enum 추가
    
    * 네비게이션 바 통합
    
    * 탭바 커스터마이징 완료
    
    * 네비게이션 바텀 바 컴포넌트 수정 완료
    
    * 추천껨프페이지 그림 완료
    
    * 메인 공통 테마 적용
    
    * 추천껨프페이지 디자인 완료
    
    * 추천리스트 인덱스 적용 완료
    
    * 임시 저장
    
    * 임시저장
    
    * 신고 테이블 완성 완료
    
    * 리포트 페이지 완성
    
    * 화면 최소화 깨짐 해결 완료
    
    * 매칭통계보기 및 게임검색카테고리수정페이지 완료
    
    * 게임 카테고리 선택 페이지 완료
    
    * 임시저장
    
    * 이미지 피커 앱 사용시 충돌 문제 발생
    
    * 임시조치 완료
    
    * 로그인박스 하단버튼 크기 조절
    
    * 로그인 페이지 수정
    
    * 네비게이션 바 수정
    
    * 로그인 유저 모델 완료
    
    * 서버통신 환경 구축
    
    * build_runner 추가 및 intercepter 구현
    
    * 페이크 데이터 바인딩 완료 - 추천 껨프
    
    * retrofit 테스트 완료
    
    * 디렉토리 구조 변경
    
    * 회원가입 모델 추가 및 페이지 컨트롤러 추가
    
    * chat 수정
    
    * 추천껨프페이지 riverpod적용 완료
    
    * 디렉토리 이름 data -> domain으로 변경
    
    * 요청껨프 list 모델 하는 중
    
    * ㅡMVVM 패턴 임의 적용
    
    * 깜프 model 추가
    
    * 추천 껨프 페이크데이터로 모델 임의 구í현 완료
    
    * page->view로 수정
    
    * my_profile mvvm패턴 구성 완료
    
    Co-authored-by: BeomyoonHeo <gqd0512@naver.com>

---
+++ lib/domain/user/repository/profile_user_repository.g.dart
@@ -0,0 +1,58 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'profile_user_repository.dart';
+
+// **************************************************************************
+// RetrofitGenerator
+// **************************************************************************
+
+// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers
+
+class _ProfileUserRepository implements ProfileUserRepository {
+  _ProfileUserRepository(
+    this._dio, {
+    this.baseUrl,
+  }) {
+    baseUrl ??= 'https://reqres.in/api';
+  }
+
+  final Dio _dio;
+
+  String? baseUrl;
+
+  @override
+  Future<ProfileUser> getUserProfile({required id}) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    final _result = await _dio
+        .fetch<Map<String, dynamic>>(_setStreamType<ProfileUser>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+            .compose(
+              _dio.options,
+              '/profileUser/${id}',
+              queryParameters: queryParameters,
+              data: _data,
+            )
+            .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
+    final value = ProfileUser.fromJson(_result.data!);
+    return value;
+  }
+
+  RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
+    if (T != dynamic &&
+        !(requestOptions.responseType == ResponseType.bytes ||
+            requestOptions.responseType == ResponseType.stream)) {
+      if (T == String) {
+        requestOptions.responseType = ResponseType.plain;
+      } else {
+        requestOptions.responseType = ResponseType.json;
+      }
+    }
+    return requestOptions;
+  }
+}
