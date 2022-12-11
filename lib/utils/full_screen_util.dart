2f6213738e36d8a58615de7ab699db8c46c5c58e
BeomyoonHeo <gqd0512@naver.com>
2022-12-11 16:43:38 +0900

    타이머 3초로 수정

--- lib/utils/full_screen_util.dart
+++ lib/utils/full_screen_util.dart
@@ -11,13 +11,13 @@ void exitFullScreen() async {
   await FullScreen.exitFullScreen();
 }
 
-// 1초 마다 전체화면을 유지 하기 위해 리스닝을 돈다.
+// 2초 마다 전체화면을 유지 하기 위해 리스닝을 돈다.
 class FullscreenService {
   Timer? timer;
   static FullscreenService? _instance;
 
   FullscreenService._internal() {
-    timer = Timer.periodic(const Duration(seconds: 2), (Timer t) {
+    timer = Timer.periodic(const Duration(seconds: 3), (Timer t) {
       SystemChrome.restoreSystemUIOverlays();
     });
   }
